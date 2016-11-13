11e1abaa0efa45107b73e95b193f28881dd4da3a
Petya Petkova <petyawebdesign@gmail.com>
2016-11-13 12:45:09 +0200

    Design changes/add ViewModel Banner to all pages

---
+++ Client/CoreClient/Models/SinglePlaceViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreClient.Models
+{
+    public class SinglePlaceViewModel
+    {
+        public string PlaceName { get; set; }
+
+    }
+}
